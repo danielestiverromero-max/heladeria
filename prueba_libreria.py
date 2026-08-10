3f6d49987e2dd631c4a62aeefc3a0107f8981fe3
daniel estiver romero agudelo <daniel.estiver.romero@correounivalle.edu.co>
2026-08-10 00:31:06 -0500

    Delete prueba_libreria.py

--- prueba_libreria.py
+++
@@ -1,8 +0,0 @@
-import sys 
-sys.path.append(r'C:\Users\USER\proyecto1\libreria_personal') 
-import misFunciones 
-print('La libreria se importo correctamente') 
-print(dir(misFunciones))
-
-from misFunciones.recursividad import sumarn
-print(sumarn(5))
