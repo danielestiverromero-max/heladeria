c10c4435448e085fb81c0d7918244f2450a20b37
daniel estiver romero agudelo <daniel.estiver.romero@correounivalle.edu.co>
2026-08-10 00:59:41 -0500

    Delete import tkinter as tk.py

--- import tkinter as tk.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-
-# crear ventana principal
-ventana = tk.Tk()
-ventana.title("Sistema de Recepción de Hotel")
-ventana.geometry("700x600")  # ancho x alto en píxeles
-
-# bucle principal (mantiene la ventana abierta)
-ventana.mainloop()
